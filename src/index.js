c1c1ad09f59cade82aab40dd1b1b6053209c1df7
Hinamoure <julia.debray@gmail.com>
2021-03-16 15:32:05 +0100

    Ouverture et fermeture de la modale. Ajout des images de pokémon dans la modale.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import './styles/index.scss';
 
 const pokeballs = document.getElementsByClassName('pokeball');
-// const pkmModal = document.getElementById('pokemon-modal');
+const pkmModal = document.getElementById('pokemon-modal');
 
-// // TODO : Attention : revoir la fermeture de la modale plus tard.
-// pkmModal.addEventListener('click', () => {
-//     pkmModal.open = false;
-// })
+pkmModal.addEventListener('click', (event) => {
+    if(event.target === event.currentTarget) {
+        event.target.open = false;
+        event.target.lastElementChild.remove();
+    }
+});
 
 //fonction de définition de l'ID du pokémon:
 const openPokeball = () => {
     for(const pokeball of pokeballs) {
         pokeball.addEventListener('click', (event) => {
+            const image = document.createElement('img');
             const pokemonId = event.currentTarget.dataset.id;
+            image.src = `./src/images/pokemon-${pokemonId}.png`;
+            pkmModal.append(image);
+            pkmModal.open = true;
             fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
             .then((response) => {
                 return response.json();
